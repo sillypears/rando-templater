8f96bcf9876b6a727c40ab45cb189f78a8a12389
sillypears <sillypears@users.noreply.github.com>
2023-02-04 01:41:15 -0500

    time2save

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Another dumb idea
 
 The overlay image (your template) should have the outside of it filled in with some color, with the sculpt itself transparent. That means you need a PNG or something similar. Cool.
+
+## v2
+v2 is optimized for breaking the template into layers. Fill each layer in with black on the outside of the template. The overlay should be filled in white to the edges.
--- main.py
+++ main.py
@@ -1,34 +1,50 @@
 from PIL import Image
 from random import randint
-import os
+import os,sys
 
-IMAGE = '.png'
-NAME = IMAGE.split('.')[0:-1][0]
-overlay = Image.open(IMAGE)
-coloroutput = Image.new("RGBA", (overlay.height, overlay.width))
-overlay = overlay.convert("RGBA")
-coloroutput = coloroutput.convert("RGBA")
+NAME = 'Daisy_Clear_ColorwayTemplate'
 
-coloroutput.paste((255,255,255), (0,0, coloroutput.width, coloroutput.height))
-total_colors = randint(10,30)
-total_images = randint(30,50)
+NUMFILES = len(os.listdir(NAME))
 
-folder = f"outputs/{NAME}{randint(0,101010101)}/"
-os.makedirs(folder)
-
-color_list = []
-for image in range(0, total_images):
-    for colors in range(0, total_colors):
-
-        color = (randint(0,255), randint(0,255), randint(0,255))
-        outImage = coloroutput.paste(
-            color,
-            (randint(0, overlay.width), randint(0, overlay.height), randint(0, overlay.width), randint(0, overlay.height))
-        )
-        color_list.append(color)
-    coloroutput.paste(overlay, (0,0), overlay)
-    coloroutput.save(f"{folder}/outimage{image}.png")
+print(f"There are {NUMFILES} layers")
 
 
+if not os.path.exists(f"{NAME}/outputs"):
+    os.makedirs(f"{NAME}/outputs")
 
-print(f"There were a total of {total_images} made")
+
+overlay = Image.open(f"{NAME}/overlay.png")
+canvas = Image.new("RGBA", (overlay.height, overlay.width))
+
+for filenum in os.listdir(NAME):
+    layer = Image.open(f"{NAME}\{filenum}")
+    Image.new("RGBA", (layer.height, layer.width))
+
+# overlay = Image.open(IMAGE)
+# coloroutput = Image.new("RGBA", (overlay.height, overlay.width))
+# overlay = overlay.convert("RGBA")
+# coloroutput = coloroutput.convert("RGBA")
+
+# coloroutput.paste((255,255,255), (0,0, coloroutput.width, coloroutput.height))
+# total_colors = randint(10,30)
+# total_images = randint(30,50)
+
+# folder = f"outputs/{NAME}{randint(0,101010101)}/"
+# os.makedirs(folder)
+
+# color_list = []
+# for image in range(0, total_images):
+#     for colors in range(0, total_colors):
+
+#         color = (randint(0,255), randint(0,255), randint(0,255))
+#         outImage = coloroutput.paste(
+#             color,
+#             (randint(0, overlay.width), randint(0, overlay.height), randint(0, overlay.width), randint(0, overlay.height))
+#         )
+#         color_list.append(color)
+#     coloroutput.paste(overlay, (0,0), overlay)
+#     coloroutput.save(f"{folder}/outimage{image}.png")
+
+
+
+# print(f"There were a total of {total_images} made")
