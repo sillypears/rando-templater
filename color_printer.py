2d3284e8b8b1ad1d6cc5e4afd05ca3e6ce6bb133
sillypears <sillypears@users.noreply.github.com>
2023-04-05 17:00:46 -0400

    A very basic color picker

---
+++ color_printer.py
@@ -0,0 +1,30 @@
+import main as m
+from colorsys import hls_to_rgb
+from random import randint
+import sys
+import requests, argparse
+
+OUTPUT_FOLDER = 'color_swatches'
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--number', dest="total", type=int, default=5, help="how many colors :D")
+    
+    args = parser.parse_args()
+    uri = "www.thecolorapi.com"
+    
+    colors = []
+    hsl1 = m.set_hsl_color()
+    
+    for x in range(args.total):                     
+        if ((randint(1,500) * x) % args.total) == 0: 
+            colors.append(m.find_adj_hsl_color(hsl1))
+        else:
+            colors.append(m.find_opp_hsl_color(hsl1))
+    # print([hls_to_rgb(x.hue, x.lum, x.sat) for x in colors])
+    for color in colors:
+        print(requests.get(f"https://{uri}/id?format=html&hsl={color.to_string()}").url)
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from random import randint, choice, random
 import os,glob
+import sys
 from natsort import natsorted
 import cv2
 import json
@@ -28,6 +29,9 @@ class HSL_Color(object):
     def to_string(self) -> str:
         return f"hsl({self.hue}, {self.sat}%, {self.lum}%)"
 
+    def to_tuple(self) -> tuple:
+        return (self.hue, self.sat, self.lum)
+    
 class RGB_Color(object):
     def __init__(self, r, g, b, a):
         self.red = r
@@ -37,6 +41,9 @@ class RGB_Color(object):
 
     def to_string(self) -> str:
         return (self.red, self.blu, self.gre, self.alp)
+
+    def to_tuple(self) -> tuple:
+        return (self.r, self.g, self.g, self.alp)
 
     def __str__(self) -> str:
         return f"({self.red}, {self.blu}, {self.gre}, {self.alp})"
@@ -81,69 +88,72 @@ def find_opp_hsl_color(color: HSL_Color) -> HSL_Color:
     n_s = (randint(HSL_OPP_THRESH_LOW,HSL_OPP_THRESH_HIGH)+color.sat)%360
     n_l = (randint(HSL_OPP_THRESH_LOW,HSL_OPP_THRESH_HIGH)+color.lum)%360
     return HSL_Color((randint(HSL_OPP_THRESH_LOW,HSL_OPP_THRESH_HIGH)+color.hue)%360, color.sat, color.lum)
- 
-parser = argparse.ArgumentParser()
+def main():
 
-parser.add_argument('-n', '--num', default=1, dest="HOWMANY", help="How many times to run")
-parser.add_argument('-s', '--sculpt', dest="SCULPTNAME", required=True, help="The name of the folder that has the layers to use")
-parser.add_argument('-m', '--model', dest="COLORMODEL", choices=["rgb", "hsl"], default="rgb", help="Uses random rgb or adjacent hsl values. Defaults rgb")
-args = parser.parse_args()
+    parser = argparse.ArgumentParser()
 
-NAME = f"{args.SCULPTNAME}"
-LAYERS = glob.glob(f"sculpts/{NAME}/layer*.png")
-NUMFILES = len(LAYERS)
+    parser.add_argument('-n', '--num', default=1, dest="HOWMANY", help="How many times to run")
+    parser.add_argument('-s', '--sculpt', dest="SCULPTNAME", required=True, help="The name of the folder that has the layers to use")
+    parser.add_argument('-m', '--model', dest="COLORMODEL", choices=["rgb", "hsl"], default="rgb", help="Uses random rgb or adjacent hsl values. Defaults rgb")
+    args = parser.parse_args()
 
-print(f"running {args.HOWMANY} times.")
-# for x in range(int(args.HOWMANY)):
-for x in progressbar.progressbar(range(int(args.HOWMANY))):
-    OUTPUTFOLDERNUM = 1
-    OUTPUTFOLDER = f"sculpts/{NAME}/output{OUTPUTFOLDERNUM}"
-    if glob.glob(f"sculpts/{NAME}/output*"):
-        OUTPUTFOLDERNUM = int("".join([x for x in natsorted(glob.glob(f"sculpts/{NAME}/output*"))[-1] if x.isdigit()]))
-        OUTPUTFOLDER = f"sculpts/{NAME}/output{OUTPUTFOLDERNUM+1}"
+    NAME = f"{args.SCULPTNAME}"
+    LAYERS = glob.glob(f"sculpts/{NAME}/layer*.png")
+    NUMFILES = len(LAYERS)
 
-    os.makedirs(f"{OUTPUTFOLDER}")
-    if not os.path.exists(f"sculpts/{NAME}/finals"): os.mkdir(f"sculpts/{NAME}/finals")
+    print(f"running {args.HOWMANY} times.")
+    # for x in range(int(args.HOWMANY)):
+    for x in progressbar.progressbar(range(int(args.HOWMANY))):
+        OUTPUTFOLDERNUM = 1
+        OUTPUTFOLDER = f"sculpts/{NAME}/output{OUTPUTFOLDERNUM}"
+        if glob.glob(f"sculpts/{NAME}/output*"):
+            OUTPUTFOLDERNUM = int("".join([x for x in natsorted(glob.glob(f"sculpts/{NAME}/output*"))[-1] if x.isdigit()]))
+            OUTPUTFOLDER = f"sculpts/{NAME}/output{OUTPUTFOLDERNUM+1}"
 
-    overlay = Image.open(f"sculpts/{NAME}/overlay.png").convert("RGBA")
-    canvas = Image.new("RGBA", (overlay.width, overlay.height))
-    color_list = []
-    imagenum = 0
+        os.makedirs(f"{OUTPUTFOLDER}")
+        if not os.path.exists(f"sculpts/{NAME}/finals"): os.mkdir(f"sculpts/{NAME}/finals")
 
-    color = set_rgb_color() if args.COLORMODEL == "rgb" else set_hsl_color()
-    for filenum in natsorted(glob.glob(f"sculpts/{NAME}/layer*.png")):
-        imagenum += 1
-        layer = Image.open(f"{filenum}").convert("RGBA")
-        layer_color = Image.new("RGBA", (layer.width, layer.height))
-        outImage = layer_color.paste(
-                color.to_string(),
-                (0,0, layer.width, layer.height)
-            )
-        color_list.append(color)
-        layer_color.paste(layer, (0,0), layer)
-        layer_color.save(f"{OUTPUTFOLDER}/outimage{imagenum}.png")
-        src = cv2.imread(f"{OUTPUTFOLDER}/outimage{imagenum}.png")
-        tmp = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-        _, alpha = cv2.threshold(tmp, 0, 255, cv2.THRESH_BINARY)
-        b, g, r = cv2.split(src)
-        rgba = [b, g, r, alpha]
-        dst = cv2.merge(rgba, 4)
-        cv2.imwrite(f"{OUTPUTFOLDER}/outimage{imagenum}.png", dst)
-        if randint(0,1):
-            color = find_adj_rgb_color(choice(color_list)) if args.COLORMODEL == "rgb" else find_adj_hsl_color(choice(color_list))
-        else:
-            color = find_opp_rgb_color(choice(color_list)) if args.COLORMODEL == "rgb" else find_opp_hsl_color(choice(color_list))
+        overlay = Image.open(f"sculpts/{NAME}/overlay.png").convert("RGBA")
+        canvas = Image.new("RGBA", (overlay.width, overlay.height))
+        color_list = []
+        imagenum = 0
 
-    # print(",".join(x.to_string() for x in color_list))
-        # print(f"Overlaying {filenum} with {color} and saving as outimage{imagenum}.png")
-    outimage = Image.new("RGBA", (overlay.width, overlay.height))
-    for filenum in glob.glob(f"{OUTPUTFOLDER}/*"):
-        layer = Image.open(filenum)
-        outimage.paste(layer, (0,0), layer)
+        color = set_rgb_color() if args.COLORMODEL == "rgb" else set_hsl_color()
+        for filenum in natsorted(glob.glob(f"sculpts/{NAME}/layer*.png")):
+            imagenum += 1
+            layer = Image.open(f"{filenum}").convert("RGBA")
+            layer_color = Image.new("RGBA", (layer.width, layer.height))
+            outImage = layer_color.paste(
+                    color.to_string(),
+                    (0,0, layer.width, layer.height)
+                )
+            color_list.append(color)
+            layer_color.paste(layer, (0,0), layer)
+            layer_color.save(f"{OUTPUTFOLDER}/outimage{imagenum}.png")
+            src = cv2.imread(f"{OUTPUTFOLDER}/outimage{imagenum}.png")
+            tmp = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+            _, alpha = cv2.threshold(tmp, 0, 255, cv2.THRESH_BINARY)
+            b, g, r = cv2.split(src)
+            rgba = [b, g, r, alpha]
+            dst = cv2.merge(rgba, 4)
+            cv2.imwrite(f"{OUTPUTFOLDER}/outimage{imagenum}.png", dst)
+            if randint(0,1):
+                color = find_adj_rgb_color(choice(color_list)) if args.COLORMODEL == "rgb" else find_adj_hsl_color(choice(color_list))
+            else:
+                color = find_opp_rgb_color(choice(color_list)) if args.COLORMODEL == "rgb" else find_opp_hsl_color(choice(color_list))
 
-    outimage.paste(overlay, (0,0), overlay)
-    outimage.save(f"{OUTPUTFOLDER}/final.png")
-    outimage.save(f"sculpts/{NAME}/finals/{NAME}{OUTPUTFOLDERNUM}.png")
-    with open (f"{OUTPUTFOLDER}/colors.txt", "w") as w:
-        w.write("\n".join(x.__str__() for x in color_list))
-        
+        # print(",".join(x.to_string() for x in color_list))
+            # print(f"Overlaying {filenum} with {color} and saving as outimage{imagenum}.png")
+        outimage = Image.new("RGBA", (overlay.width, overlay.height))
+        for filenum in glob.glob(f"{OUTPUTFOLDER}/*"):
+            layer = Image.open(filenum)
+            outimage.paste(layer, (0,0), layer)
+
+        outimage.paste(overlay, (0,0), overlay)
+        outimage.save(f"{OUTPUTFOLDER}/final.png")
+        outimage.save(f"sculpts/{NAME}/finals/{NAME}{OUTPUTFOLDERNUM}.png")
+        with open (f"{OUTPUTFOLDER}/colors.txt", "w") as w:
+            w.write("\n".join(x.__str__() for x in color_list))
+            
+if __name__ == "__main__":
+    sys.exit(main())
